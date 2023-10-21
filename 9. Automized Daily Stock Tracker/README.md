57f404a8741dc47a5885b01274c4b22b4dce033f
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2023-10-20 19:39:01 -0700

    "Added Automized Daily Stock Tracker Files"

---
+++ 9. Automized Daily Stock Tracker/README.md
@@ -0,0 +1,37 @@
+# Automized Daily Stock Tracker App [[Website here]](https://mysqlfinancialdatabase-3oest9jb8p63gsf2fyqdxb.streamlit.app/)
+## Project Description
+<img src="images/Financedb_strmlit.png">
+This project aims to create a dynamic data dashboard to visualize the top 20 gainers, losers, and most actively traded tickers in the US market, utilizing data from the AlphaVantage API. This dashboard updates daily, providing a clear and concise view of market movers.
+
+## Data Source
+
+The data is retrieved from the AlphaVantage API, specifically using the [TOP_GAINERS_LOSERS](https://www.alphavantage.co/documentation/#gainer-loser) function to fetch the top gainers, losers, and the most actively traded tickers in the US market.
+
+## Project Structure
+
+- data/: Directory for storing data files.
+- src/:
+  - data_retrieval.py: Script to fetch data from AlphaVantage API and store it in a PostgreSQL database.
+  - <s>data_processing.py: Script to process the data for visualization.</s>
+- dashboard/:
+  - app.py: Streamlit application to create the interactive dashboard.
+- db_setup.sql: Script documenting the database schema creation.
+
+## Setup and Installation
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory.
+3. Install the required dependencies using the following command:
+   `pip install -r requirements.txt`
+4. Set up the environment variables for database connection and API access in a .env file as follows:
+   `ALPHA_VANTAGE_API_KEY=your_api_key_here` <br>
+    `SUPABASE_URL=your_url` <br>
+    `SUPABASE_SERVICE_ROLE_KEY=your_service_role_key`<br>
+5. Run the data_retrieval.py script to fetch and store the data.
+6. Run the Streamlit app using the following command:
+   `streamlit run dashboard/app.py`
+7. Using Github Actions to automate fetching the data from AlphaVantage - create .github/worklows/dashboard_update.yml file
+
+## Usage
+
+Upon launching the Streamlit app, you can select a date using the date picker widget to view the top gainers, losers, and most actively traded tickers for that specific day. The data displayed includes the ticker symbol, price, change amount, change percentage, and trading volume.
---
+++ 9. Automized Daily Stock Tracker/app.py
@@ -0,0 +1,93 @@
+import streamlit as st
+#import psycopg2
+import pandas as pd
+import plotly.express as px
+from streamlit_extras.stoggle import stoggle
+import supabase_py 
+from dotenv import load_dotenv
+import os
+
+# Load and get db Environment Variables
+load_dotenv()
+supabase_url = os.getenv('SUPABASE_URL')
+supabase_key = os.getenv('SUPABASE_SERVICE_ROLE_KEY')
+supabase = supabase_py.create_client(supabase_url, supabase_key)
+
+# Page config
+st.set_page_config(
+    layout="wide",
+    page_title="Finance Dashboard",
+    page_icon="📈"
+)
+
+#@st.cache
+def fetch_data(table_name, date):
+  date_str = date.strftime('%Y-%m-%d')
+  result = supabase.table(table_name).select().eq('date', date_str).execute()
+  #print(result) # print results
+  data = pd.DataFrame(result['data'])
+  return data
+
+# Custom text fonts
+def custom_css():
+    st.markdown("""
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f4f4f4;
+            }
+            .stTable {
+                font-size: 14px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+# Plot our data
+def plot_data(df, title, y_column):
+    fig = px.bar(df, x='ticker', y=y_column, title=title, labels={'ticker': 'Ticker', y_column: y_column.capitalize()}, color=y_column, color_continuous_scale=px.colors.sequential.Plasma)
+    st.plotly_chart(fig)
+
+
+def main():
+    custom_css()
+    st.title("Kevin's Automized Daily Stock Tracker📈")
+
+    stoggle(
+        "Click me!",
+        """ This Dashboard refreshes daily using Github Actions, by running a Python script to automate fetching and storing data in an SQL Database, displaying the top 20 gainers, losers and most actively traded financial stocks of the day from the S&P 500, Nasdaq, and Crypto market.
+        """
+    )
+
+    
+    date = st.date_input('**Select a Date** (Data begins on date of project creation 2023/10/16)', value=pd.to_datetime('today') - pd.Timedelta(days=1))  # Date Picker - CHANGE To show 'yesterday' to avoid issues
+
+    st.header(f"Top 20 Gainers on {date.strftime('%Y-%m-%d')}")
+    top_gainers = fetch_data("top_gainers", date)
+    cols = st.columns(2)  # Create two columns
+    with cols[0]:
+        st.write(top_gainers)  # Display table in the first column
+    with cols[1]:
+        #fig = px.bar(top_gainers, x='ticker', y='change_percentage', title='Top 20 Gainers')
+        plot_data(top_gainers, 'Top 20 Gainers', 'change_percentage')  # Display chart in the second column
+
+    st.header(f"Top 20 Losers on {date.strftime('%Y-%m-%d')}")
+    top_losers = fetch_data("top_losers", date)
+    cols = st.columns(2)
+    with cols[0]:
+        st.write(top_losers)
+    with cols[1]:
+        #fig = px.bar(top_losers, x='ticker', y='change_percentage', title='Top 20 Losers')
+        plot_data(top_losers, 'Top 20 Losers', 'change_percentage')
+
+    st.header(f"Top 20 Most Actively Traded {date.strftime('%Y-%m-%d')}")
+    most_actively_traded = fetch_data("most_actively_traded", date)
+    cols = st.columns(2)
+    with cols[0]:
+        st.write(most_actively_traded)
+    with cols[1]:
+        #fig = px.bar(most_actively_traded, x='ticker', y='volume', title='Top 20 Most Actively Traded')
+        plot_data(most_actively_traded, 'Top 20 Most Actively Traded', 'volume')
+
+if __name__ == "__main__":
+  main()
+
